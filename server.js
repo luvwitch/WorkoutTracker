440f03872f16d86098aa4889e5a85843f90e3849
luvwitch <itsalexkelly@gmail.com>
2021-01-08 14:25:56 -0800

    changed connect for heroku

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.use(express.static("public"));
 require("./routes/api")(app);
 require("./routes/view")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the server
 app.listen(PORT, () => {
